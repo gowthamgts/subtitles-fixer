7f182e087e7aed061f511db1e3de048b7539415f
Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
2016-06-17 14:42:20 +0530

    added some deps

--- index.js
+++ index.js
@@ -7,4 +7,26 @@
  * @author Gowtham Gopalakrishnan <gowthamgts12@gmail.com>
  */
 
-var arguments = process.argv.slice(2);
+const fs = require('fs');
+const path = require('path');
+const chalk = require('chalk');
+
+
+// Supporting variables
+const videoFormats = ['.mp4', '.mkv', '.avi']
+
+// TODO: cleanup this code.
+const argv = require('minimist')(process.argv.slice(2));
+console.dir(argv);
+console.log(chalk.bgBlue(`Currently Processing: ${argv.path}`));
+
+var dirFiles = fs.readdirSync(argv.path);
+var videoFiles = [];
+
+for (var i in dirFiles) {
+	if (videoFormats.indexOf(path.extname(dirFiles[i])) != -1) {
+		videoFiles.push(dirFiles[i]);
+	}
+}
+
+console.log(videoFiles);
